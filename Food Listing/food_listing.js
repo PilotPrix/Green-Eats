dd8bce4b09cc4e8067f5cd471e74ed33025c997d
= <69162332+PilotPrix@users.noreply.github.com>
2021-08-21 11:20:49 -0400

    "Add" and "Delete" buttons get their job done

--- Food Listing/food_listing.js
+++ Food Listing/food_listing.js
@@ -5,13 +5,21 @@ function Add(){
     listingsNum += 1
 
     //Add listing after the one before
-    listings.innerHTML += '<h3>Food Item ' + listingsNum + '</h3> <input type="text" name="food item" placeholder="Enter food item (eg. Apples)" required> <input type="number" name="quantity" placeholder="Enter amount" required> <input type="number" name="price" placeholder="Price $$ for each" required> <br>'
-    //Add delete if it isn't added already
-    if(listingsNum == 2){
-        foodAdder.innerHTML += '<button type="button" onclick="Delete()">Delete</button>'
-    }
+    foodAdder.innerHTML += '<div id="Listing ' + listingsNum + '" class="Listings"> <h3>Food Item ' + listingsNum + '</h3> <input type="text" name="food item" placeholder="Enter food item (eg. Apples)" required> <input type="number" name="quantity" placeholder="Enter amount" required> <input type="number" name="price" placeholder="Price $$ for each" required> <button type="button" onclick="Delete(' + listingsNum + ')">Delete</button> <br> </div>'
 }
 
-function Delete(){
-    alert("Deleted")
+function Delete(n){
+    //Delete listing
+    const listing = document.getElementById("Listing " + n)
+    listing.remove()
+    listingsNum -= 1
+
+    const listings = document.getElementsByClassName("Listings")
+    //rename all listings below deleted one
+    for(let i = n - 1; i < listings.length; i++){
+        console.log(i)
+        listings[i].id = "Listing " + (i + 1)
+        listings[i].getElementsByTagName("h3")[0].innerHTML = "Food Item " + (i + 1)
+        console.log(listings[i].getElementsByTagName("Button")[0].setAttribute("onclick", "Delete(" + (i + 1) + ")"))
+    }
 }
