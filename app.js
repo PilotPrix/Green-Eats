3083d4c62b7885e2cdee9fb196093230e534b404
Harryyoshi <75763081+Harryyoshi@users.noreply.github.com>
2021-08-21 13:53:46 -0400

    Fixed link to food listing

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-//var Food_ListingRouter = require('./routes/Food Listing');
+var food_listingRouter = require('./routes/food_listing');
 
 var app = express();
 
@@ -22,6 +22,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/food_listing', food_listingRouter);
 //app.use('/Food Listing', Food_ListingRouter);
 
 // catch 404 and forward to error handler
