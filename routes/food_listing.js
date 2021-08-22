b15b0154d4a5c86af4bfcea22cfe5fbf5f2208da
Harryyoshi <75763081+Harryyoshi@users.noreply.github.com>
2021-08-21 22:27:32 -0400

    Data write to json file

--- routes/food_listing.js
+++ routes/food_listing.js
@@ -8,29 +8,41 @@ router.get('/', function(req, res, next) {
 });
 
 router.post('/', function(req, res) {
-  let food = []
-  let quantity = []
-  let price = []
+    let food = []
+    let quantity = []
+    let price = []
+    /*const food = req.body["food_item_1"];
+    const quantity = req.body["quantity_1"];
+    const price = req.body["price_1"];*/
 
-  let i = 0
+    let i = 0
   
-  while ("food_item_" + (i + 1) in req.body) {
-    console.log(req.body)
-    try{
-      food[i] = req.body["food_item_" + (i + 1)]
-      quantity[i] = req.body["quantity_" + (i + 1)]
-      price[i] = req.body["price_" + (i + 1)]
+    while ("food_item_" + (i + 1) in req.body) {
+        console.log(req.body)
+        try{
+            food[i] = req.body["food_item_" + (i + 1)]
+            quantity[i] = req.body["quantity_" + (i + 1)]
+            price[i] = req.body["price_" + (i + 1)]
+        }
+        catch{
+            break
+        }
+        i++;
     }
-    catch{
-      break
-    }
-    i++;
-  }
 
-  const address = req.body["location"]
-  const company = req.body["company"]
-
-  res.json({food, quantity, price, address, company})
+    const address = req.body["location"]
+    const company = req.body["company"]
+    
+    const jsonOut = {food,quantity,price};
+    fs.appendFile("jsons/food_listing.json", JSON.stringify({food,quantity,price,address,company}) + "\n", function (err) {
+        if(err) {
+            console.log("Error adding listing");
+            return console.log(err);
+        } else {
+            console.log("Listing added");
+        }
+    });
+    res.render('listing_confirm', { title: 'Express' });
 })
 
 module.exports = router;
